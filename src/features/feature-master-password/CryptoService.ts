72725a031fa04efe9bc4a591badcf9cc7c670eca
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2023-08-08 16:26:55 +0200

    Fix hash

--- src/features/feature-master-password/CryptoService.ts
+++ src/features/feature-master-password/CryptoService.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
-import sha256 from 'crypto-js/sha256';
+import CryptoJS from 'crypto-js';
 import {Notice} from "obsidian";
 
 export function getHash(password: string): string {
-    return sha256(sha256(password))
+    return CryptoJS.SHA256(CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex)).toString(CryptoJS.enc.Hex)
 }
